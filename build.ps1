7858fc3772fa1c64ee1fe14f264895c8229e0832
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-08-30 12:50:31 -0700

    moving projects around

--- build.ps1
+++ build.ps1
@@ -53,7 +53,7 @@ function Build-Projects{
     [cmdletbinding()]
     param()
     process {
-        $projectToBuild = Join-Path $scriptDir 'src\GeoffreyTrx\GeoffreyTrx.sln'
+        $projectToBuild = Join-Path $scriptDir 'src\Geoffrey.sln'
 
         if(-not (Test-Path $projectToBuild)){
             throw ('Could not find the project to build at [{0}]' -f $projectToBuild)
@@ -68,7 +68,7 @@ function Clean{
     [cmdletbinding()]
     param()
     process {
-        [System.IO.FileInfo]$projectToBuild = Join-Path $scriptDir 'src\GeoffreyTrx\GeoffreyTrx.sln'
+        [System.IO.FileInfo]$projectToBuild = Join-Path $scriptDir 'src\Geoffrey.sln'
 
         if(-not (Test-Path $projectToBuild)){
             throw ('Could not find the project to build at [{0}]' -f $projectToBuild)
@@ -76,7 +76,7 @@ function Clean{
 
         Invoke-MSBuild $projectToBuild -visualStudioVersion 14.0 -targets Clean -properties @{'DeployExtension'='false'}
 
-        [System.IO.DirectoryInfo[]]$foldersToDelete = (Join-Path $scriptDir 'src\GeoffreyTrx\GeoffreyTrx\bin\'),(Join-Path $scriptDir 'src\GeoffreyTrx\GeoffreyTrx\obj\')
+        [System.IO.DirectoryInfo[]]$foldersToDelete = (Join-Path $scriptDir 'src\GeoffreyTrx\bin\'),(Join-Path $scriptDir 'src\GeoffreyTrx\obj\')
         foreach($folder in $foldersToDelete){
             if(Test-Path $folder){
                 Remove-Item $folder -Recurse -Force
