6de37d4feaa4582e605dabfc97e0d07834194de5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-09-04 11:42:06 -0700

    disabling coverage as there is a CI issue

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,6 +1,6 @@
 ﻿$env:ExitOnPesterFail = $true
 $env:IsDeveloperMachine=$true
-$env:PesterEnableCodeCoverage = $true
+#$env:PesterEnableCodeCoverage = $true
 
 if($env:APPVEYOR_REPO_BRANCH -eq "release"){
     .\build.ps1 -publishToNuget
