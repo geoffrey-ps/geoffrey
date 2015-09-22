dabc8dd6f0e1e3638094d669bf02d2e3c8481264
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-09-22 00:52:39 -0700

    updating to version 0.0.7-beta

--- getgeoffrey.ps1
+++ getgeoffrey.ps1
@@ -56,7 +56,7 @@ function GetPsModulesPath2{
 try{
     EnsureNuGetPowerShellInstlled2
     # get the module locally
-    $installpath = (Get-NuGetPackage -name geoffrey -version 0.0.6-beta -binpath)
+    $installpath = (Get-NuGetPackage -name geoffrey -version 0.0.7-beta -binpath)
 
     # copy the files to the powershell modules folder
     $destFolderPath = (Join-Path (GetPsModulesPath2) 'geoffrey')
